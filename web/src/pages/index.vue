aeb550d117706e81de42900a475632bb9ba9b1f9
Phloemus <brieucquemeneur@gmail.com>
2026-01-22 17:04:08 +0100

    build: adding tags to the datasets cards

--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -7,14 +7,24 @@
 <template>
     <div>
         <section>
-            <h2 class="my-6 font-semibold text-slate-900 text-xl">Available research data</h2>
+            <h2 class="my-6 font-semibold text-slate-900 text-xl">Available metagenomics data</h2>
             <div class="grid grid-cols-3 gap-4">
                 <div
                     v-for="(dataset, index) in datasets.data.items"
                     class="p-4 relative bg-slate-100 rounded-md border-2 border-slate-100 hover:border-sky-500 hover:cursor-pointer"
                 >
                     <h3 class="font-semibold text-md text-slate-800 truncate">{{ dataset.name }}</h3>
-                    <p class="mt-4 mb-14 text-slate-600 text-md line-clamp-4">
+                    <div class="mt-2 flex gap-2">
+                        <div class="px-2 py-1 bg-sky-200 text-sky-500 text-sm rounded-sm">Dataset</div>
+                        <NuxtLink 
+                            :to="dataset.url"
+                            v-if="dataset.global_id"
+                            class="px-2 py-1 bg-purple-200 text-purple-500 text-sm hover:underline rounded-sm"
+                        >
+                            {{ dataset.global_id }}
+                        </NuxtLink>
+                    </div>
+                    <p class="mt-6 mb-16 text-slate-600 text-md line-clamp-4">
                         {{ dataset.description }}
                     </p>
                     <div class="absolute bottom-4 flex gap-2">
