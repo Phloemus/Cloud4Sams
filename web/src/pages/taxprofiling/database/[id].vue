2c5fdda282f3cabea735d828dbb1ff0b0a66bd53
Debian <vivek.ashokan@inrae.fr>
2026-09-02 11:55:23 +0000

    correct syntax lang=ts

--- web/src/pages/taxprofiling/database/[id].vue
+++ web/src/pages/taxprofiling/database/[id].vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { useTaxProfiling } from '~/composables/useTaxProfiling'
 import DatabaseCard from '~/components/DatabaseCard.vue'
 
--- web/src/pages/taxprofiling/tool/[id].vue
+++ web/src/pages/taxprofiling/tool/[id].vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { useTaxProfiling } from '~/composables/useTaxProfiling'
 import ToolCard from '~/components/ToolCard.vue'
 
