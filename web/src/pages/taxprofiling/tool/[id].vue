be545a2653a73c931e61c9c9d9eb10e3a088a35b
Vivek Ashokan <vashokan@mgps.inra.fr>
2026-09-18 14:53:37 +0200

    add biotools link to more information, correct id access

--- web/src/pages/taxprofiling/tool/[id].vue
+++ web/src/pages/taxprofiling/tool/[id].vue
@@ -97,7 +97,7 @@ const loading = ref(false)
                 :repo="tool.repo"
                 :doi="tool.doi"
                 :documentation="tool.documentation"
-                :id="tool.id"
+                :id="tool['@id']"
               />
             </div>
 
