2099ac3b60633ba9fd0b7d972c21e541d2ca8a33
Vivek Ashokan <vashokan@mgps.inra.fr>
2026-09-22 18:11:11 +0200

    add indexes to tools and datasets

--- web/src/pages/dataset/index.vue
+++ web/src/pages/dataset/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="w-full flex flex-col items-center justify-center py-32">
+  <div class="flex flex-col w-full items-center">
+    
+    <div class="text-3xl font-bold text-slate-900">
+        Datasets
+    </div>
 
-    <h1 class="text-3xl font-bold text-slate-900">
-      Datasets
-    </h1>
-
-    <h2 class="mt-4 text-lg text-slate-600">
+    <div class="mt-4 text-lg text-slate-600">
       This page is not available yet.
-    </h2>
+    </div>
 
-    <div class="mt-6 px-4 py-2 bg-slate-100 border border-slate-200 rounded-md text-slate-600">
+    <div class="mt-6 px-4 py-2 bg-slate-100 border border-slate-200 rounded-md">
       Coming soon
     </div>
 
