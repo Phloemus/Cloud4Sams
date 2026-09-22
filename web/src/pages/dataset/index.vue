8f07d494923dadb3c4188d9b5e8940586ff8d81f
Vivek Ashokan <vashokan@mgps.inra.fr>
2026-09-22 18:00:32 +0200

    add indexes to tools and datasets

--- web/src/pages/dataset/index.vue
+++ web/src/pages/dataset/index.vue
@@ -10,12 +10,8 @@ definePageMeta({
       Datasets
     </h1>
 
-    <p class="mt-4 text-lg text-slate-600">
+    <h2 class="mt-4 text-lg text-slate-600">
       This page is currently under construction.
-    </p>
-
-    <div class="mt-6 px-4 py-2 bg-slate-100 border border-slate-200 rounded-md text-slate-600">
-      Coming soon
-    </div>
+    </h2>
   </div>
 </template>
